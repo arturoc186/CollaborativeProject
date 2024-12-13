3b573b38ee42dc17eafc8b7a48a1c0f5f926a2b2
arturoc186 <81912907+arturoc186@users.noreply.github.com>
2024-12-13 12:10:57 +0100

    Create ListadoDeTareas.java

---
+++ ListadoDeTareas.java
@@ -0,0 +1,44 @@
+public class ListaDeTareas {
+    public static void main(String[] args) {
+        ListaDeTareas listaDeTareas = new ListaDeTareas();
+        int seleccion_tarea;
+
+        do {
+            System.out.println("--- Lista de Tareas ---");
+            System.out.println("1. Agregar tarea");
+            System.out.println("2. Marcar tarea como completada");
+            System.out.println("3. Listar todas las tareas");
+            System.out.println("4. Listar tareas pendientes");
+            System.out.println("5. Listar tareas completadas");
+            System.out.println("6. Salir");
+            System.out.print("Elige una opción: ");
+           seleccion_tarea = ConsoleReader.readInt();
+
+            switch (seleccion_tarea) {
+                case 1:
+                    System.out.print("Ingrese la descripción de la tarea: ");
+                    String descripcion = ConsoleReader.readString();
+                    listaDeTareas.agregarTarea(descripcion);
+                    break;
+                case 2:
+                    listaDeTareas.listarTareas();
+                    System.out.print("Ingrese el índice de la tarea a marcar como completada: ");
+                    int indice = ConsoleReader.readInt();
+                    listaDeTareas.marcarTareaComoCompletada(indice);
+                    break;
+                case 3:
+                    listaDeTareas.listarTareas();
+                    break;
+                case 4:
+                    listaDeTareas.listarTareasPendientes();
+                    break;
+                case 5:
+                    listaDeTareas.listarTareasCompletadas();
+                    break;
+                case 6:
+                    System.out.println("Has salido correctamente del programa. ¡Hasta pronto!");
+                    break;
+            }
+        } while (opcion != 6);
+    }
+}
